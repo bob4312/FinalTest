dc857a03b17fd5dbc31d0ba822e776de9102a8f5
bob <MineStop36@gmail.com>
2025-05-26 13:49:42 +0000

    test22

--- front-app/src/views/HomeView.vue
+++ front-app/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-import audioChat from '../components/AudioChat.vue'
+// import audioChat from '../components/AudioChat.vue'
 </script>
 
 <template>
-    <!-- <h1 style="color: red;">IT IS WORKING !!!!</h1> -->
-     <audioChat></audioChat>
+
 </template>
--- front-app/src/views/HostVIew.vue
+++ front-app/src/views/HostVIew.vue
@@ -2,6 +2,7 @@
   <div style="padding:2rem; max-width:600px; margin:auto;">
     <!-- reuse your Host component -->
     <Host />
+    <h4>hello</h4>
   </div>
 </template>
 
